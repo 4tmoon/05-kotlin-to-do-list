ab251f9d0e7e887bec57c94eca3c720bfa02c611
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-25 20:53:33 -0300

    feat: adiciona projeto inicial

---
+++ app/src/main/java/com/github/atmoon/to_do_list/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.github.atmoon.to_do_list
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.github.atmoon.to_do_list.navigation.AppNavigation
+import com.github.atmoon.to_do_list.ui.theme.TodolistTheme
+import com.github.atmoon.to_do_list.viewmodel.TarefaViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TodolistTheme {
+                val viewModel: TarefaViewModel = viewModel(
+                    factory = TarefaViewModel.factory(applicationContext)
+                )
+                AppNavigation(viewModel = viewModel)
+                }
+            }
+        }
+    }
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TodolistTheme {
+        Greeting("Android")
+    }
+}
